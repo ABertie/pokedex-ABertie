80bf9cf807d3b59ccd3be02e67ac923457c2e8a5
ABertie <U2439307@rts-365.dk>
2023-09-12 12:54:37 +0200

    pokeDex without search funtion

---
+++ pokemon.js
@@ -0,0 +1,64 @@
+const URL = new URLSearchParams(window.location.search)
+
+const SCREEN = document.querySelector(".pokemonScreen")
+const SHINY = document.querySelector(".buttonShiny")
+
+const SPINNER = document.querySelector(".spinner")
+
+SCREEN.innerHTML = `
+    <h2 class="pokmonName">* *</h2>
+    <img class="pokeImg" src="" alt="screen">
+    <div class="disc"></div>
+    <h2 class="pokeID"><i class="fa-solid fa-bars"></i></h2>`
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${URL.get("name")}`)
+    .then(function(response) {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            document.body.innerText="Ups, noget gik galt. Prøv igen senere"
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+        
+        SPINNER.style.display = "none"
+        
+        const DIV = document.querySelector(".pokemon")
+
+        DIV.innerHTML = `
+        <section class="pokeTypes">
+            <h3 class="pokeTypes__header">Types:</h3>        
+            <ul class="pokeTypes__list">
+            ${data.types.map(elem => `<li class="${elem.type.name}">${elem.type.name}</li>`).join("")}
+            </ul>
+        </section>
+        <div class="pokeSize">
+            <p>Gns. height: ${data.height / 10} m</p>
+            <p>Gns. weight: ${data.weight / 10} kg</p>
+        </div>
+        `
+
+        const IMG = new Image()
+        IMG.src = data.sprites.other["official-artwork"].front_default
+        IMG.classList = "pokeImg"
+        IMG.alt = data.name
+
+        IMG.onload = function () {
+            SCREEN.querySelector(".pokeImg").append(IMG)
+        }
+
+        SCREEN.innerHTML = `
+        <h2 class="pokmonName">${data.name}</h2>
+        <!-- <img class="pokeImg" src="${data.sprites.other["official-artwork"].front_default || "" }" alt="${data.name}"> -->
+        <span class="pokeImg" src="" alt="${data.name}" style="padding:0;"></span>
+        <div class="disc"></div>
+        <h2 class="pokeID">#${data.id}</h2>
+        `
+
+        SHINY.addEventListener("click", function() {
+            const IMG = SCREEN.querySelector(".pokeImg")
+            if (IMG.src.includes("shiny")) IMG.src = data.sprites.other["official-artwork"].front_default || ""
+            else IMG.src = data.sprites.other["official-artwork"].front_shiny || ""
+        })
+    })
